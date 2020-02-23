ecfad08d1e3b367c62c57af95d82a02c674b1ea2
Andre Saddler <dresadd09691@gmail.com>
2020-02-23 12:46:34 -0500

    add User Input to PHP script
    $(urlfetch https://ServerName.herokuapp.com/apexstats.php?platform=YourPlatformHere&nick=YourUsernameHere)

--- apexstats.php
+++ apexstats.php
@@ -2,15 +2,30 @@
 header('Content-type: text/plain');
 
 $apikey = '';
-$platform = 'PC';
-$player = 'Rehkloos';
+
+$platform = strtolower($_GET['platform']);
+	if (!in_array($platform, array('origin', 'xbl', 'psn'))) { echo '\'&platform=\' parameter not correct! (Options: \'xbl\', \'psn\' or \'origin\')'; return; };
+
+	if ($platform == 'origin') {
+		$machine = 'PC';
+	};
+	if ($platform == 'xbl') {
+		$machine = 'X1';
+	};
+	if ($platform == 'psn') {
+		$machine = 'PS4';
+	};
+
+$player = $_GET['nick'];
+	if (!$player) { echo '\'&nick=\' parameter not defined!'; return; };
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, 'https://api.mozambiquehe.re/bridge?version=4&platform='.$platform.'&player='.$player.'&auth='.$apikey);
+curl_setopt($ch, CURLOPT_URL, 'https://api.mozambiquehe.re/bridge?version=4&platform='.$machine.'&player='.$player.'&auth='.$apikey);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 $result = json_decode(curl_exec($ch), true);
+
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
@@ -20,4 +35,4 @@ echo " $player Apex Rank: ".$result['global']['rank']['rankName']." ".$result['g
 
 // More Explained Rank info output
 // echo "Apex Rank: ".$result['global']['rank']['rankName']." ".$result['global']['rank']['rankDiv']." ";
-// cho "Score: ".$result['global']['rank']['rankScore'];
+// echo "Score: ".$result['global']['rank']['rankScore'];
