4ee354b7042595cd5a4867a00e6cb2a8bdde681a
Andre Saddler <dresadd09691@gmail.com>
2020-02-23 19:14:55 -0500

    add command parameter to php script
    - Specify a command after the *&command=* query parameter at the end of the URL in the $(urlfetch) method.   
    In the example above I specified "*rank*"  or "*stats*" as for the current rank in the current season.  
    
    - need to add &command= after nickname to pull up specific rank or general stats
    
    - beautify php script

--- apexstats.php
+++ apexstats.php
@@ -3,36 +3,92 @@ header('Content-type: text/plain');
 
 $apikey = '';
 
-$platform = strtolower($_GET['platform']);
-	if (!in_array($platform, array('origin', 'xbl', 'psn'))) { echo '\'&platform=\' parameter not correct! (Options: \'xbl\', \'psn\' or \'origin\')'; return; };
+$request = strtolower($_GET['command']);
+if (!$request)
+{
+    echo '\'&command=\' parameter not defined! (Options: \'rank\', \'stats\' or \'time\')';
+    return;
+};
 
-	if ($platform == 'origin') {
-		$machine = 'PC';
-	};
-	if ($platform == 'xbl') {
-		$machine = 'X1';
-	};
-	if ($platform == 'psn') {
-		$machine = 'PS4';
-	};
+$platform = strtolower($_GET['platform']);
+if (!in_array($platform, array(
+    'origin',
+    'xbl',
+    'psn'
+)))
+{
+    echo '\'&platform=\' parameter not correct! (Options: \'xbl\', \'psn\' or \'origin\')';
+    return;
+};
+
+if ($platform == 'origin')
+{
+    $machine = 'PC';
+};
+if ($platform == 'xbl')
+{
+    $machine = 'X1';
+};
+if ($platform == 'psn')
+{
+    $machine = 'PS4';
+};
 
 $player = $_GET['nick'];
-	if (!$player) { echo '\'&nick=\' parameter not defined!'; return; };
+if (!$player)
+{
+    echo '\'&nick=\' parameter not defined!';
+    return;
+};
 
-$ch = curl_init();
+function _getJSON($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json, text/plain, */*', 'Authorization: Ubi_v1 t='.apcu_fetch('uplayconnect_ticket'), 'Ubi-AppId: 39baebad-39e5-4552-8c25-2c9b919064e2', 'Connection: keep-alive', 'Keep-Alive: timeout 0, max 0'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // so curl_exec returns data
+    // grab URL and pass it to the browser; store returned data
+    $curlRes = curl_exec($ch);
 
-curl_setopt($ch, CURLOPT_URL, 'https://api.mozambiquehe.re/bridge?version=4&platform='.$machine.'&player='.$player.'&auth='.$apikey);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if (curl_errno($ch))
+    {
+        echo 'Error:' . curl_error($ch);
+    }
 
-$result = json_decode(curl_exec($ch), true);
+    // close cURL resource, and free up system resources
+    curl_close($ch);
+    // Decode returned JSON so it can be handled as a multi-dimensional associative array
+    return json_decode($curlRes, true);
+};
 
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close ($ch);
+if ($request == 'stats')
+{
 
-echo " $player Apex Rank: ".$result['global']['rank']['rankName']." ".$result['global']['rank']['rankDiv']." || ".$result['global']['rank']['rankScore'];
+    $data = _getJSON('https://api.mozambiquehe.re/bridge?version=4&platform=' . $machine . '&player=' . $player . '&auth=' . $apikey);
 
-// More Explained Rank info output
-// echo "Apex Rank: ".$result['global']['rank']['rankName']." ".$result['global']['rank']['rankDiv']." ";
-// echo "Score: ".$result['global']['rank']['rankScore'];
+    // Total Apex Kills
+    $kills = intval($data['total']['kills']['value']);
+    // Total Apex Dmg
+    $dmg = intval($data['total']['damage']['value']);
+    // Total Apex Games Played
+    $games_played = intval($data['total']['games_played']['value']);
+    // Current Apex Level
+    $lvl = intval($data['global']['level']);
+    // Kill/Death Ratio
+    $adr = round($dmg / $games_played, 2);
+
+    // echo 'Lv.'.$lvl['player_profiles'][0]['level'].' | '.$wlRatio.' W/L ratio | '.$kdRatio.' K/D ratio';
+    echo "$player Stats: Lv. " . $lvl . " | Lifetime Kills: " . $kills . " | Lifetime Damage " . $dmg . " | Games Played: " . $games_played . " | ADR: " . $adr . " ";
+};
+
+if ($request == 'rank')
+{
+
+    $rank = _getJSON('https://api.mozambiquehe.re/bridge?version=4&platform=' . $machine . '&player=' . $player . '&auth=' . $apikey);
+    echo " $player Apex Rank: " . $rank['global']['rank']['rankName'] . " " . $rank['global']['rank']['rankDiv'] . " || " . $rank['global']['rank']['rankScore'] . " RP";
+
+    // More Explained Rank info output
+    // echo "Apex Rank: ".$result['global']['rank']['rankName']." ".$result['global']['rank']['rankDiv']." ";
+    // echo "Score: ".$result['global']['rank']['rankScore'];
+    
+};
